7fba0094dee37f3af6a94a0b78c8972f63154155
mayurhapani <hapanimayur@gmail.com>
2024-01-24 15:15:29 +0530

    added netlify configuration

---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: "src/index.js", // Specify the path to your entry module
+  output: {
+    file: "dist/bundle.js", // Specify the output file
+    format: "iife", // Specify the format (e.g., 'iife' for a standalone script)
+  },
+};

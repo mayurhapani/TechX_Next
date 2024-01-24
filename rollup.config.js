3b6da3f63a026cc574fa6757a85ef063ded8d7b5
mayurhapani <hapanimayur@gmail.com>
2024-01-24 15:38:05 +0530

    restore old settings

--- rollup.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  input: "src/index.js", // Specify the path to your entry module
-  output: {
-    file: "dist/bundle.js", // Specify the output file
-    format: "iife", // Specify the format (e.g., 'iife' for a standalone script)
-  },
-};
